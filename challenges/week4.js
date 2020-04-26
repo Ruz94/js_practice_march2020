609a52b6294755c26e37fee50c4ccb729fe27af4
ruzina75@gmail.com <ruzina75@gmail.com>
2020-04-26 22:53:42 +0100

    Week 4 Challenge Complete

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,43 +1,63 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+
+  return nums.filter((n) => n < 1);
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+
+  return names.filter((n) => {
+    if (n.charAt(0) === char) {
+      return n;
+    }
+  });
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+
+  return words.filter((w) => {
+    if (w.includes("to ")) {
+      return w;
+    }
+  });
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+
+  return nums.filter((num) => Number.isInteger(num));
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+
+  return users.map((u) => u.data.city.displayName);
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+
+  return nums.map((n) => {
+    return Math.round(Math.sqrt(n) * 100) / 100;
+  });
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+
+  return sentences.filter((item) => item.toLowerCase().includes(str.toLowerCase()));
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+
+  return triangles.map((t) => {
+    return Math.max(...t);
+  });
 }
 
 module.exports = {
@@ -48,5 +68,5 @@ module.exports = {
   getCities,
   getSquareRoots,
   findSentencesContaining,
-  getLongestSides
+  getLongestSides,
 };
--- test/week4.test.js
+++ test/week4.test.js
@@ -6,7 +6,7 @@ const {
   getCities,
   getSquareRoots,
   findSentencesContaining,
-  getLongestSides
+  getLongestSides,
 } = require("../challenges/week4");
 
 describe("findSmallNums", () => {
@@ -21,11 +21,7 @@ describe("findSmallNums", () => {
 describe("findNamesBeginningWith", () => {
   test("returns an array of names beginning with the specified character", () => {
     const names = ["Sally", "Dave", "Susan", "Geoff", "Riley", "Sam"];
-    expect(findNamesBeginningWith(names, "S")).toEqual([
-      "Sally",
-      "Susan",
-      "Sam"
-    ]);
+    expect(findNamesBeginningWith(names, "S")).toEqual(["Sally", "Susan", "Sam"]);
     expect(findNamesBeginningWith(names, "D")).toEqual(["Dave"]);
     expect(findNamesBeginningWith(names, "F")).toEqual([]);
   });
@@ -33,27 +29,13 @@ describe("findNamesBeginningWith", () => {
 
 describe("findVerbs", () => {
   test("returns an array of words that are considered verbs (because they begin with 'to ')", () => {
-    const words = [
-      "to eat",
-      "fajita",
-      "mouse",
-      "to sneak",
-      "to squeak",
-      "cheesemonger"
-    ];
+    const words = ["to eat", "fajita", "mouse", "to sneak", "to squeak", "cheesemonger"];
     expect(findVerbs(words)).toEqual(["to eat", "to sneak", "to squeak"]);
     expect(findVerbs(["bottle", "fish", "grain"])).toEqual([]);
   });
 
   test("does not mistake words that include 'to' elsewhere", () => {
-    const words = [
-      "to eat",
-      "tower",
-      "monitor",
-      "to sneak",
-      "to squeak",
-      "cheesemonger"
-    ];
+    const words = ["to eat", "tower", "monitor", "to sneak", "to squeak", "cheesemonger"];
     expect(findVerbs(words)).toEqual(["to eat", "to sneak", "to squeak"]);
   });
 });
@@ -75,37 +57,37 @@ describe("getCities", () => {
         data: {
           city: {
             id: 1,
-            displayName: "MCR"
-          }
-        }
+            displayName: "MCR",
+          },
+        },
       },
       {
         id: 44,
         data: {
           city: {
             id: 4,
-            displayName: "LVP"
-          }
-        }
+            displayName: "LVP",
+          },
+        },
       },
       {
         id: 5,
         data: {
           city: {
             id: 4,
-            displayName: "LVP"
-          }
-        }
+            displayName: "LVP",
+          },
+        },
       },
       {
         id: 2,
         data: {
           city: {
             id: 7,
-            displayName: "GLW"
-          }
-        }
-      }
+            displayName: "GLW",
+          },
+        },
+      },
     ];
     expect(getCities(users)).toEqual(["MCR", "LVP", "LVP", "GLW"]);
   });
@@ -129,37 +111,43 @@ describe("findSentencesContaining", () => {
     "If you plan to publish your package, the most important things in your package.json are the name and version fields as they will be required",
     "If you don’t plan to publish your package, the name and version fields are optional",
     "Put keywords in it. It's an array of strings. This helps people discover your package as it's listed in npm search",
-    "The bugs field should hold the url to your project’s issue tracker and / or the email address to which issues should be reported."
+    "The bugs field should hold the url to your project’s issue tracker and / or the email address to which issues should be reported.",
   ];
 
   test("returns only the sentences containing the specified string", () => {
-    expect(
-      findSentencesContaining(sentencesAboutPackageJson, "license")
-    ).toEqual([
-      "You should specify a license for your package so that people know how they are permitted to use it"
+    expect(findSentencesContaining(sentencesAboutPackageJson, "license")).toEqual([
+      "You should specify a license for your package so that people know how they are permitted to use it",
     ]);
 
-    expect(
-      findSentencesContaining(sentencesAboutPackageJson, "binary")
-    ).toEqual([]);
+    expect(findSentencesContaining(sentencesAboutPackageJson, "binary")).toEqual([]);
   });
 
   test("it should not be case sensitive", () => {
-    expect(
-      findSentencesContaining(sentencesAboutPackageJson, "dependencies")
-    ).toEqual([
+    expect(findSentencesContaining(sentencesAboutPackageJson, "dependencies")).toEqual([
       "Dependencies are specified in a simple object that maps a package name to a version range",
-      "It's best to map these additional items such as a test framework, which is not needed for running your project, in a devDependencies object"
+      "It's best to map these additional items such as a test framework, which is not needed for running your project, in a devDependencies object",
     ]);
   });
 });
 
 describe("getLongestSides", () => {
   test("returns the longest side of each set of triangle data", () => {
-    const data = [[6, 7, 10], [9, 3, 6], [6, 3, 5], [6, 13, 12], [7, 12, 8]];
+    const data = [
+      [6, 7, 10],
+      [9, 3, 6],
+      [6, 3, 5],
+      [6, 13, 12],
+      [7, 12, 8],
+    ];
     expect(getLongestSides(data)).toEqual([10, 9, 6, 13, 12]);
 
-    const data2 = [[6, 7, 7], [9, 3, 9], [6, 3, 5], [6, 13, 12], [7, 12, 8]];
+    const data2 = [
+      [6, 7, 7],
+      [9, 3, 9],
+      [6, 3, 5],
+      [6, 13, 12],
+      [7, 12, 8],
+    ];
     expect(getLongestSides(data2)).toEqual([7, 9, 6, 13, 12]);
   });
 });
